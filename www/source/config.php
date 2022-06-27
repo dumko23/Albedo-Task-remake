1fba36cb1565299ac39953b4b49ef3e8be6bb032
Dima Parkhomenko <parhomenko.dmit@gmail.com>
2022-06-27 13:00:08 +0300

    Refactoring credentials using .env

--- www/source/config.php
+++ www/source/config.php
@@ -3,14 +3,21 @@
 namespace App;
 
 use PDO;
+use Dotenv;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
 return [
     'database' => [
-        'dbAndTable' => "MemberList.Members",
-        'user' => 'root',
-        'password' => 'password',
-        'name' => 'mysql:host=mysql;port:3306;',
-        'db' => 'MemberList',
+        'dbAndTable' => $_ENV['MYSQL_DATABASE'] . '.' . $_ENV['MYSQL_TABLE'],
+        'user' => $_ENV['MYSQL_USER'],
+        'password' => $_ENV['MYSQL_PASSWORD'],
+        'name' => 'mysql:host=' . $_ENV['MYSQL_HOSTNAME'] . ';port:' . $_ENV['MYSQL_PORT'] . ';',
+        'db' => $_ENV['MYSQL_DATABASE'],
         'options' => [
             PDO::ATTR_DEFAULT_FETCH_MODE => 2
         ],
